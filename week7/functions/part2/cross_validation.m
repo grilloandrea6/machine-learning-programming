19b35043ee3d9cc5a06b2b50cba593d84332ebfb
Andrea Grillo <andrea.grillo@epfl.ch>
2023-11-12 11:18:08 +0100

    cleaned cross validation code

--- week7/functions/part2/cross_validation.m
+++ week7/functions/part2/cross_validation.m
@@ -20,36 +20,6 @@ function [avgTP, avgFP, stdTP, stdFP] =  cross_validation(X, y, F_fold, valid_ra
 %
 %% %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-% 
-% K = length(params.k_range);
-% FP_ = zeros(1,F_fold);
-% TP_ = zeros(1,F_fold);
-% 
-% avgTP = zeros(1,K);
-% avgFP = zeros(1,K);
-% stdTP = zeros(1,K);
-% stdFP = zeros(1,K);
-% 
-% 
-% for k = 1:K
-%     params.k = params.k_range(k);
-% 
-%     for i = 1:F_fold
-%         [X_train, y_train, X_test, y_test] = split_data(X, y, valid_ratio);
-% 
-%         y_est = knn(X_train,  y_train, X_test, params);
-%         C = confusion_matrix(y_test, y_est);
-%         TP_(i) = C(1,1)/(C(1,1) + C(1,2));
-%         FP_(i) = C(2,1)/(C(2,1)+C(2,2));
-%     end
-% 
-%     avgTP(k) = mean(TP_);
-%     avgFP(k) = mean(FP_);
-%     stdTP(k) = std(TP_);
-%     stdFP(k) = std(FP_);
-% end
-
-
 TP_ = zeros(F_fold,length(params.k_range));
 FP_ = zeros(F_fold,length(params.k_range));
 
@@ -59,7 +29,6 @@ for f = 1:F_fold
     [ tproc, fproc ] = knn_ROC( X_train, y_train, X_test, y_test,  params );
     TP_(f,:) = tproc(:);
     FP_(f,:) = fproc(:);
-
 end
 
 avgTP = mean(TP_);
